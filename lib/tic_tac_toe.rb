a182041003902624a93bfb1cc464c644da154ee2
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-04-02 16:30:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,23 @@ def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+
 def turn_count(board)
   count = 0
   board.each do |index|
